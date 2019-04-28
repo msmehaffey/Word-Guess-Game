8532eaa36453970a5e8d57d3d1e490e9d1a09a5c
Shuler <msmehaf2@gmail.com>
2019-04-28 11:07:56 -0500

    No more being notified multiple times after completion, style changes, new names added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,40 @@
-var randomWords = ["squibbs", "squibbles", "brad", "bradley", "leroy", "elroy", "muffin", "cutieguy", "scooter", "winslow", "pumpkin"];
+var randomWords = ["squibbs", "squibbles", "brad", "bradley", "leroy", "elroy", "muffin", "cutieguy", "scooter", "winslow", "pumpkin", "biscuit", "bradford"];
 var blankWord = [];
 
 
- document.body.onkeypress = function findRandom() {
-
+ function findRandom() {
     document.getElementById("headliner").innerHTML = "Start Guessing!"
-
-
      var i = Math.floor(Math.random() * randomWords.length);
      var wordSelect = randomWords[i];
      var randArr = wordSelect.split("");
      var guessRemaining = 15;
 
-     for (var d = 0; d < wordSelect.length; d++){
-     blankWord.push("_");
-     document.getElementById("challenge").innerHTML = blankWord.join(" ");
+        for (var d = 0; d < wordSelect.length; d++){
+        blankWord.push("_");
+        document.getElementById("challenge").innerHTML = blankWord.join(" ");
 
-        document.body.onkeypress = function(event) {
-            var guess = String.fromCharCode(event.keyCode)
-            document.getElementById("theGuess").innerHTML += guess;
-            guessRemaining--;
-            document.getElementById("guessLeft").innerHTML = guessRemaining;
-            if (guessRemaining === 0) {
-                document.location.reload(false);
-                alert("You ran out of guesses! The name was: " + wordSelect);
-            }
+            document.body.onkeypress = function(event) {
+                var guess = String.fromCharCode(event.keyCode)
+                document.getElementById("theGuess").innerHTML += guess;
+                guessRemaining--;
+                document.getElementById("guessLeft").innerHTML = guessRemaining;
+                    for (var j = 0; j < wordSelect.length; j++) {
 
-            for (var j = 0; j < wordSelect.length; j++) {
-                if (randArr[j] === guess) {
-                blankWord[j] = guess;
-                console.log(blankWord.indexOf("_"));
-                }
-                if (blankWord.indexOf("_") === -1) {
-                    document.location.reload(false);
-                    alert("You won! The name was: " + wordSelect + "!");
-                }
-            }
+                        if (randArr[j] === guess) {
+                            blankWord[j] = guess;
+                            console.log(blankWord.indexOf("_"));
+                        }
+                    }
+                    if (guessRemaining === 0) {
+                        document.location.reload(false);
+                        alert("You ran out of guesses! The name was: " + wordSelect);
+                    }
+                    if (blankWord.indexOf("_") === -1) {
+                            document.location.reload(false);
+                            alert("You won! The name was: " + wordSelect + "!");
+                    }
                 document.getElementById("challenge").innerHTML = blankWord.join(" ");
-                }
+            }
 
         } 
   
